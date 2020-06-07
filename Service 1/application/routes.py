aa1c5516d6880857ae4a26238fef45a874bfea33
Hamza Yacub <hamzayacub97@gmail.com>
2020-06-07 23:51:00 +0000

    added service 1 logic

--- Service 1/application/routes.py
+++ Service 1/application/routes.py
@@ -1,6 +1,12 @@
+from application import app, db
 from flask import render_template, redirect, url_for, request
+from application.models import Discounts
+from requests import get
 
-@app.route('/')
-@app.route('/home')
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/home', methods=['GET', 'POST'])
 def home():
-    return render_template('home.html')
+    discountsData = Discounts.query.all()
+    discount = requests.get('http://service_4:5004').text
+    
+    return render_template('home.html', title='Home', discount_str=discount, discount=discountsData)
