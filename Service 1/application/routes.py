ae0121e608f1aa86298a4dac753bf8007805cf15
Hamza Yacub <hamzayacub97@gmail.com>
2020-06-07 23:58:01 +0000

    added route for generate page

--- Service 1/application/routes.py
+++ Service 1/application/routes.py
@@ -7,6 +7,11 @@ from requests import get
 @app.route('/home', methods=['GET', 'POST'])
 def home():
     discountsData = Discounts.query.all()
-    discount = requests.get('http://service_4:5004').text
     
-    return render_template('home.html', title='Home', discount_str=discount, discount=discountsData)
+    return render_template('home.html', title='Home', discount=discountsData)
+
+@app.route('/generate', methods=['GET', 'POST'])
+def generate():
+    discount = requests.get('http://service_4:5004').text
+
+    return render_template('generate.html', title='Generate discount', discount_response=discount)
