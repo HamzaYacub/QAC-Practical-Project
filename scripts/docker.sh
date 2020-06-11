e236caa35567461ce32443cd4ddfd8f78096c9d3
Hamza Yacub <hamzayacub97@gmail.com>
2020-06-11 09:31:58 +0000

    added build command in pipeline

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -6,4 +6,14 @@ env SFIA2_DB_URI="${SFIA2_DB_URI}"
 
 env SECRET_KEY="${SECRET_KEY}"
 
+docker-compose build
+
+docker push hamzay97/sfia2_service_1:latest
+
+docker push hamzay97/sfia2_service_2:latest
+
+docker push hamzay97/sfia2_service_3:latest
+
+docker push hamzay97/sfia2_service_4:latest
+
 docker stack deploy --compose-file docker-compose.yaml stack1
